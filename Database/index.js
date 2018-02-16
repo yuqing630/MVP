2620f861d3398ec12630cee4ce36a9ea7a724fd0
Hack Reactor Student <student@hackreactor.com>
2018-02-16 12:07:48 -0500

    basic setup

---
+++ Database/index.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+});
+
+var listSchema = mongoose.Schema({
+  id: {type: Number, unique: true},
+  name: String,
+  stat: String,
+  url: String,
+  like: Boolean
+
+
+
+});
+
+var List = mongoose.model('List', listSchema);
+
+export.save = (lists) => {
+  return list.create(lists)
+}
+
+
+var selectAll = () => {
+  return List.find({like : true})
+  .sort('id')
+  .exec()
+};
+
+module.exports.selectAll = selectAll;
---
+++ Server/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
+// var items = require('../database-mysql');
+var items = require('../Database');
+
+var app = express();
+
+// UNCOMMENT FOR REACT
+app.use(express.static(__dirname + '/../Client/dist'));
+
+
+
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
